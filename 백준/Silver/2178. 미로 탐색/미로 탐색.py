b432af9c50f3ce5669b5fc7e930f8b183093caa6
gaeunnlee <gaeunnnleee@gmail.com>
2025-08-08 03:07:14 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,40 +1,26 @@
 from collections import deque
-import sys
 
 N, M = map(int, input().split(" "))
 graph = []
-
 dy, dx = [1, 0, -1, 0], [0, 1, 0, -1]
-distance_map = [[sys.maxsize] * M for _ in range(0, N)]
-visited = [[False] * M for _ in range(0, N)]
-queue = deque()
-
 
 for _ in range(0, N):
     graph.append(list(map(int, input())))
 
 
-def bfs(graph, visited, distance_map):
-    visited[0][0] = True
-    distance_map[0][0] = 1
-    queue.append((0, 0, 1))
+def bfs(startY, startX, graph):
+    queue = deque()
+    queue.append((startY, startX))
     while queue:
-        y, x, d = queue.popleft()
+        y, x = queue.popleft()
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
-            if (
-                ny <= -1
-                or ny >= N
-                or nx <= -1
-                or nx >= M
-                or visited[ny][nx]
-                or graph[ny][nx] == 0
-            ):
+            if ny <= -1 or ny >= N or nx <= -1 or nx >= M or graph[ny][nx] == 0:
                 continue
-            visited[ny][nx] = True
-            distance_map[ny][nx] = min(distance_map[ny][nx], d + 1)
-            queue.append((ny, nx, d + 1))
+            if graph[ny][nx] == 1:  # 방문하지 않은 경우
+                graph[ny][nx] = graph[y][x] + 1
+                queue.append((ny, nx))
+    return graph[startY - 1][startX - 1]
 
 
-bfs(graph, visited, distance_map)
-print(distance_map[N - 1][M - 1])
+print(bfs(0, 0, graph))
