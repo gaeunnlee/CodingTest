27c0314475849ae4a4375ed0cdf0ba73860dda1a
gaeunnlee <gaeunnnleee@gmail.com>
2025-11-07 09:19:07 +0900

    [level 2] Title: 주식가격, Time: 39.01 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> st = new Stack<>();
+        
+        for ( int i = 0; i < prices.length; i++ ) {       
+            while (!st.isEmpty() && prices[st.peek()] > prices[i]) {
+               int top = st.pop();
+               answer[top] = i - top;                
+            }
+            
+            st.push(i);
+        }
+        
+        while (!st.isEmpty()) {
+            int top = st.pop();
+            answer[top] = prices.length - 1 - top;
+        }
+        
+        return answer;
+    }
+}
