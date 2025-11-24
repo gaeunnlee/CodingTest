cad6fb33f0944a3d25aa7af364f01f752c543c2e
gaeunnlee <gaeunnnleee@gmail.com>
2025-11-24 10:25:56 +0900

    [level 1] Title: 폰켓몬, Time: 7.16 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,37 +1,12 @@
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int canChocie = nums.length / 2;
-        int choiceCnt = 0;
-        Set<Integer> choiceSet = new HashSet<>();
-        Map<Integer, Integer> poncketmon = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         
-        for (int n : nums) {
-            poncketmon.put(n, poncketmon.getOrDefault(n, 0) + 1);
-        }
+        for (int n : nums) set.add(n);
         
-        int keySize = poncketmon.keySet().size(); 
-        
-        if (keySize >= canChocie) {
-            return canChocie;
-        } else {
-            while (choiceCnt < canChocie) {
-                for (Map.Entry<Integer,Integer> entry: poncketmon.entrySet()) {
-                    int key = entry.getKey();
-                    int value = entry.getValue();
-                    if (value == 0) continue;
-                    poncketmon.put(key, entry.getValue() - 1);
-                    choiceSet.add(key);
-                    choiceCnt++;
-                }
-                
-            }
-        }
-        
-        return choiceSet.size();
+        return Math.min(set.size(), nums.length / 2);
     }
 }
