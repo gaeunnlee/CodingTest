915bc31a75484b1c82efd7addce495e5f1bd1f1e
gaeunnlee <gaeunnnleee@gmail.com>
2025-11-25 14:07:47 +0900

    [level 0] Title: 배열 조각하기, Time: 16.71 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        List<Integer> list = new ArrayList<>(Arrays.stream(arr).boxed().collect(Collectors.toList()));
+        
+        for ( int i = 0 ; i < query.length; i++) {
+                if (i % 2 == 0) {
+                    list = list.subList(0, query[i] + 1);
+                } else {
+                   list = list.subList(query[i], list.size());
+                }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
