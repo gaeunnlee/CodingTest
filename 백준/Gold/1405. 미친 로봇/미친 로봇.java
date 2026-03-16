5fe7b5c3619229f2c1a24cee61482171d616de36
gaeunnlee <gaeunnnleee@gmail.com>
2026-03-16 18:28:12 +0900

    [Gold IV] Title: 미친 로봇, Time: 300 ms, Memory: 190732 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.java
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.java
@@ -1,52 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
+class Main {
+    
     static int n;
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         double[] p = new double[4];
         boolean[][] visited = new boolean[2 * n + 1][2 * n + 1];
-
+        
         for (int i = 0; i < 4; i++) {
-            p[i] = (double) Integer.parseInt(st.nextToken()) / 100;
+            p[i] = Integer.parseInt(st.nextToken()) / 100.0 ;
         }
-
+        
         visited[n][n] = true;
-        double answer = backtrack(n, n, 1.0, 0, p, visited);
-
-
-        System.out.println(answer);
+        System.out.println(backtrack(n, n, p, visited, 1.0, 0));
+        
+        
+        
     }
-
-    static double backtrack(int y, int x, double current, int cnt, double[] p, boolean[][] visited) {
+    
+    
+    static double backtrack(int y, int x, double[] p, boolean[][] visited, double cur, int cnt) {
+        
         double result = 0;
-        if (cnt == n) {
-            return current;
+        
+        if (n == cnt) {
+            return cur;
         }
-
-        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
+        
+        int[][] dir = {{0, 1}, {0, -1}, { 1, 0 }, {-1, 0}};
+        
         for (int i = 0; i < 4; i++) {
             if (p[i] == 0) continue;
-
+            
             int ny = y + dir[i][0];
             int nx = x + dir[i][1];
-
-            if (ny < 0 || ny > 2 * n || nx < 0 || nx > 2 * n) continue;
-
+            
+            if ( ny < 0 || ny > 2 * n || nx < 0 || nx > 2 * n) continue;
             if (visited[ny][nx]) continue;
+            
             visited[ny][nx] = true;
-            result += backtrack(ny, nx, current * p[i], cnt + 1, p, visited);
+            
+            result += backtrack(ny, nx, p, visited, cur * p[i], cnt + 1);
+            
             visited[ny][nx] = false;
+            
         }
-
+        
         return result;
+        
     }
 }
