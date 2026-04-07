b21ee67e986b26ad1dd9410e709a46793bad563e
gaeunnlee <gaeunnnleee@gmail.com>
2026-04-07 15:34:10 +0900

    [Silver III] Title: 두 수의 합, Time: 320 ms, Memory: 27036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -2,33 +2,39 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] numbers = new int[n];
-        // 서로 다른 양의 정수들이므로 key를 value로 설정해도 괜찮음
-        Map<Integer, Integer> map = new HashMap<>(); // value, index
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            numbers[i] = num;
-            map.put(num, i);
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         int x = Integer.parseInt(br.readLine());
-
+        
+        Arrays.sort(numbers);
+        
+        int left = 0;
+        int right = n - 1;
         int answer = 0;
-
-        for (int i = 0; i < n; i++) {
-            int num = numbers[i];
-            int target = x - num;
-            if (map.get(target) != null && map.get(target) > i) {
+        
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            
+            if (sum == x) {
                 answer++;
+                left++;
+                right--;
+            } else if (sum < x) {
+                left++;
+            } else {
+                right--;
             }
         }
-
+        
         System.out.println(answer);
-
+        
     }
 }
