172ec2a448e601f0f587fdca1819c3a737dc0fb2
gaeunnlee <gaeunnnleee@gmail.com>
2026-05-04 23:26:41 +0900

    [level 3] Title: 베스트앨범, Time: 4.04 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,69 +1,44 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-	public int[] solution(String[] genres, int[] plays) {
-		List<Integer> answer = new ArrayList<>();
-		HashMap<String, Genre> genreMap = new HashMap<>();
-		HashSet<Genre> album = new HashSet<>();
-
-		for (int i = 0; i < genres.length; i++) {
-			String genre = genres[i];
-			int play = plays[i];
-			Genre g;
-			if (!genreMap.containsKey(genre)) {
-				g = new Genre(genre);
-				genreMap.put(genre, g);
-				album.add(g);
-			} else {
-				g = genreMap.get(genre);
-			}
-			g.addMusic(new Music(i, play));
-		}
-
-		List<Genre> genreList = new ArrayList<>(album);
-		genreList.sort(
-				(a, b) -> b.list.stream().mapToInt(i -> i.play).sum() - a.list.stream().mapToInt(i -> i.play).sum());
-
-		for (Genre g : genreList) {
-			List<Music> list = g.list;
-			list.sort((m1, m2) -> {
-				if (m2.play == m1.play) {
-					return m1.number - m2.number;
-				}
-				return m2.play - m1.play;
-			});
-
-			int limit = list.size() == 1 ? 1 : 2;
-			for (int i = 0; i < limit; i++) {
-				answer.add(list.get(i).number);
-			}
-		}
-		System.out.println(genreList);
-		return answer.stream().mapToInt(i -> i).toArray();
-	}
-
-	static class Genre {
-		String name;
-		List<Music> list = new ArrayList<>();
-		Genre(String name) {
-			this.name = name;
-		}
-		void addMusic(Music m) {
-			list.add(m);
-		}
-	}
-
-	static class Music {
-		int number;
-		int play;
-
-		Music(int number, int play) {
-			this.number = number;
-			this.play = play;
-		}
-	}
-
+    public int[] solution(String[] genres, int[] plays) {
+        ArrayList<Integer> answerList = new ArrayList<>();
+        
+        HashMap<String, Integer> genreTotalPlayCnt = new HashMap<>();
+        HashMap<String, ArrayList<int[]>> musicsByGenre = new HashMap<>();
+        
+        for (int i = 0; i < genres.length; i++) {
+            String g = genres[i];
+            int cnt = plays[i];
+            
+            genreTotalPlayCnt.put(g, genreTotalPlayCnt.getOrDefault(g, 0) + cnt);
+            
+            if (musicsByGenre.get(g) == null) {
+                musicsByGenre.put(g, new ArrayList<int[]>());
+            }
+            
+            musicsByGenre.get(g).add(new int[]{i, cnt});
+            
+        }
+        
+        ArrayList<String> sortedGenre = new ArrayList<String>(genreTotalPlayCnt.keySet());
+        Collections.sort(sortedGenre, (a, b) -> genreTotalPlayCnt.get(b) - genreTotalPlayCnt.get(a));
+        
+        
+        for (String g: sortedGenre) {
+            ArrayList<int[]> sortedMusic = new ArrayList<int[]>(musicsByGenre.get(g));
+            Collections.sort(sortedMusic, (a, b) -> {
+                if (a[1] == b[1]) {
+                    return a[0] - b[0];
+                } else {
+                    return b[1] - a[1];
+                }
+            });
+                
+            answerList.add(sortedMusic.get(0)[0]);
+            if (sortedMusic.size() > 1) answerList.add(sortedMusic.get(1)[0]);
+        }
+        
+        return answerList.stream().mapToInt(i -> i).toArray();
+    }
 }
