a3d6faea8c2a2e7b2a01803757eac16051eb1453
gaeunnlee <gaeunnnleee@gmail.com>
2026-05-08 02:09:45 +0900

    [level 2] Title: H-Index, Time: 0.64 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,13 +4,13 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
         int n = citations.length;
-
+        
         for (int i = 0; i < n; i++) {
-            int h = n - i;              // i번째부터 끝까지 논문 수
-            if (citations[i] >= h) {    // 그 논문이 h 이상이면 h 성립
-                return h;
+            if (n - i <= citations[i]) {
+                return n - i;
             }
         }
+        
         return 0;
     }
 }
