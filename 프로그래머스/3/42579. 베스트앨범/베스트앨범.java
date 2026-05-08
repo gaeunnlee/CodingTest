a2aa7dc6be0b45493edb7bd20817bb9b6820708c
gaeunnlee <gaeunnnleee@gmail.com>
2026-05-08 14:02:25 +0900

    [level 3] Title: 베스트앨범, Time: 5.90 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,43 +2,43 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        ArrayList<Integer> answerList = new ArrayList<>();
         
-        HashMap<String, Integer> genreTotalPlayCnt = new HashMap<>();
-        HashMap<String, ArrayList<int[]>> musicsByGenre = new HashMap<>();
+        HashMap<String, Integer> genre = new HashMap<>();
+        HashMap<String, ArrayList<int[]>> music = new HashMap<>();
         
         for (int i = 0; i < genres.length; i++) {
             String g = genres[i];
             int cnt = plays[i];
             
-            genreTotalPlayCnt.put(g, genreTotalPlayCnt.getOrDefault(g, 0) + cnt);
+            genre.put(g, genre.getOrDefault(g, 0) + cnt);
+            music.computeIfAbsent(g, key -> new ArrayList<>()).add(new int[] {i, cnt});
+        }
+        
+        List<String> list = new ArrayList<>(genre.keySet());
+        list.sort((a, b) -> genre.get(b) - genre.get(a));
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        for (String g: list) {
             
-            if (musicsByGenre.get(g) == null) {
-                musicsByGenre.put(g, new ArrayList<int[]>());
+            List<int[]> musics = music.get(g);
+            musics.sort((a, b) -> {
+                if (a[1] == b[1]) {
+                    return Integer.compare(a[0], b[0]);
+                }
+                
+                return Integer.compare(b[1], a[1]);
+            });
+            
+            answer.add(musics.get(0)[0]);
+            
+            if (musics.size() > 1) {
+                answer.add(musics.get(1)[0]);
             }
             
-            musicsByGenre.get(g).add(new int[]{i, cnt});
-            
         }
         
-        ArrayList<String> sortedGenre = new ArrayList<String>(genreTotalPlayCnt.keySet());
-        Collections.sort(sortedGenre, (a, b) -> genreTotalPlayCnt.get(b) - genreTotalPlayCnt.get(a));
         
-        
-        for (String g: sortedGenre) {
-            ArrayList<int[]> sortedMusic = new ArrayList<int[]>(musicsByGenre.get(g));
-            Collections.sort(sortedMusic, (a, b) -> {
-                if (a[1] == b[1]) {
-                    return a[0] - b[0];
-                } else {
-                    return b[1] - a[1];
-                }
-            });
-                
-            answerList.add(sortedMusic.get(0)[0]);
-            if (sortedMusic.size() > 1) answerList.add(sortedMusic.get(1)[0]);
-        }
-        
-        return answerList.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
