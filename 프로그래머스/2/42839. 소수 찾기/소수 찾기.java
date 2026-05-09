b18e91b041839e2f25c72254df2598fcfba3d213
gaeunnlee <gaeunnnleee@gmail.com>
2026-05-09 22:15:18 +0900

    [level 2] Title: 소수 찾기, Time: 8.15 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -43,7 +43,7 @@ class Solution {
 	static boolean isPrime(int n) {
 		if (n <= 1) return false;
 
-		for (int i = 2; i < n; i++) if (n % i == 0) return false;
+		for (int i = 2; i <= Math.sqrt(n); i++) if (n % i == 0) return false;
 
 		return true;
 	}
